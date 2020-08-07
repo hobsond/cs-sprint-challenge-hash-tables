2c7e3d4c8705244c0bfbfe085b9936d31cb98f31
hobsond <60270270+hobsond@users.noreply.github.com>
2020-08-07 12:01:31 -0400

    ex 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,42 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    # create a variable for dictionary
+    cache={}
+    stack=[]
+    # loop through the list items
+    for i in a:
+    # if item is greater than 0 i will check to see if its in the dictionary
+        if i > 0:
+            if i not in cache:
+    # if its not in the dictionary i will add that key and = 0 
+                cache[i] = 0
+            else:
+                cache[i]+=1
+    # if its less than 0 i ill take value
+        if i < 0:
+    # and inverse and if ithe item is in the dictionary
+            x = i * -1
+            if x in cache:
+    # then ill add that key += 1
+                cache[x] +=1
+            else:
+                cache[x]= 0
+    # loop through the catch if key value > 0 the n append the key
+    # to an array
+    # return array
+    for i in cache.items():
+        if i[1] ==1:
+            stack.append(i[0])
+    
+    
+    
+    
+     
+    
+    
 
-    return result
+    return stack
 
 
 if __name__ == "__main__":
