9c6d7c950e25dba90521a69c064c400f77c11d38
hobsond <60270270+hobsond@users.noreply.github.com>
2020-09-04 16:24:24 -0400

    Update ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,42 @@
-def get_indices_of_item_weights(weights, length, limit):
+def get_indices_of_item_weights(weights, length, limit,cache=None,count=None,start =None):
     """
     YOUR CODE HERE
     """
     # Your code here
+    if cache is None:
+        cache={}
+    if count is None:
+        count = 0
+    if start is None:
+        start = 0
+    
+    while len(cache) < length* length :
+       
+        if count >= length:
+            count = 0
+            if start < length :
+                start +=1
+        
+        try:
+            added = weights[start] + weights[count]
+       
+            if added not in cache:
+                cache[added] = [(weights[start],weights[count])]
+            # else:
+            #     cache[added].append((weights[start],weights[count]))
+            count +=1
+        except IndexError:
+            break
+        
+        continue
+            
+        
+    
+    print(cache[limit])
+    
 
-    return None
+
+we = [ 4, 6, 10, 15, 16 ]
+leng = 5
+lim = 21
+get_indices_of_item_weights(we,leng,lim)
