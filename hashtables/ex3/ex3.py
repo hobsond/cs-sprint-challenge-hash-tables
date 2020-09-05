2772a2c018e4c83b7f9e5bebaea14cf94c2f4d5d
hobsond <60270270+hobsond@users.noreply.github.com>
2020-09-04 20:27:00 -0400

    stetch complete

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -9,25 +9,38 @@ def intersection(arrays):
     # create the dictionary 
     # we loop throuh arrays 
     # which we will h
-    
-    
-    for i in arrays:
-        for num in i:
-            if num not in cache:
-                cache[num] = 1
+    start =0
+    check = 0 
+    height = len(arrays)
+    while start <= height:
+        try:
+            
+            number = arrays[start][check] 
+            if number not in cache:
+                cache[number]= [start]
             else:
-                cache[num] += 1
-
-    for num in cache:
-        if cache[num] == len(arrays):
-            result.append(num)
+                cache[number].append(start)
+                if len(cache[number]) == height:
+                    result.append(number)
+            check += 1 
+        except:
+            check = 0
+            start += 1
+        
+        continue
     return result
+   
+   
     
 
 if __name__ == "__main__":
-    arrays = []
+    arrays = [
+            [1,2,3],
+            [1,4,5,3],
+            [1,6,7,3]
+        ]
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    # arrays.append(list(range(100, 200)) + [1, 2, 3])
+    # arrays.append(list(range(200, 300)) + [1, 2, 3])
+    # arrays.append(list(range(300, 400)) + [1, 2, 3])
     print(intersection(arrays))
--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,44 +2,30 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    # Your code here
-    # create a variable for dictionary
-    # cache={}
-    # stack=[]
-    # # loop through the list items
-    # for i in a:
-    # # if item is greater than 0 i will check to see if its in the dictionary
-    #     if i > 0:
-    #         if i not in cache:
-    # # if its not in the dictionary i will add that key and = 0 
-    #             cache[i] = 0
-    #         else:
-    #             cache[i]+=1
-    # # if its less than 0 i ill take value
-    #     if i < 0:
-    # # and inverse and if ithe item is in the dictionary
-    #         x = i * -1
-    #         if x in cache:
-    # # then ill add that key += 1
-    #             cache[x] +=1
-    #         else:
-    #             cache[x]= 0
-    # # loop through the catch if key value > 0 the n append the key
-    # # to an array
-    # # return array
-    # for i in cache.items():
-    #     if i[1] ==1:
-    #         stack.append(i[0])
-    
-    
-    
-    
-     
-    
-    
-
-    return stack
+    table = set()
+    savior = []
+    count = 0
+    while len(table) <= len(a):
+        try:
+            
+            x = a[count]
+            num = x *-1
+            if x not in table :
+                if x<0 and num in table:
+                    savior.append(num)
+                elif x>0 and num in table:
+                    savior.append(x)   
+                table.add(x)
+                
+        except:
+            break
+          
+        count += 1
+        continue
+    return savior
+        
+   
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    print(has_negatives([-1,-2,1,2,3,4,-4]))
